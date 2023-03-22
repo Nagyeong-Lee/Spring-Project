dad4eabbd79a80660e3776c7ab121e29a0e1925e
Nagyeong-Lee <nhd5607@gmail.com>
2023-03-23 01:52:31 +0900

    add: LogAspect

---
+++ src/main/java/com/example/Spring_Project/LogAspect.java
@@ -0,0 +1,21 @@
+package com.example.Spring_Project;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Aspect
+@Component
+public class LogAspect {
+
+    @Around("within(com.example.Spring_Project..*)")
+    public Object logging(ProceedingJoinPoint proceedingJoinPoint) throws Throwable {
+        Object result = proceedingJoinPoint.proceed();
+        log.info("==> LogAspect Root:: " + proceedingJoinPoint.getSignature().getDeclaringTypeName());
+        log.info("==> LogAspect Method:: " + proceedingJoinPoint.getSignature().getName());
+        return result;
+    }
+}
